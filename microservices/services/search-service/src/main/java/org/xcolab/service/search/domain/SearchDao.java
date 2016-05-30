9051ba6211006c083dce2349c3f324aace4ff9f3
Johannes Bachhuber <jobachhu@mit.edu>
2016-05-30 16:13:28 +0200

    [COLAB-1177] Cleaned up unfinished proposal search

--- microservices/services/search-service/src/main/java/org/xcolab/service/search/domain/SearchDao.java
+++ microservices/services/search-service/src/main/java/org/xcolab/service/search/domain/SearchDao.java
@@ -1,12 +1,10 @@
 package org.xcolab.service.search.domain;
 
 import org.xcolab.service.search.wrappers.MemberSearchResult;
-import org.xcolab.service.search.wrappers.SearchResult;
 import org.xcolab.service.utils.PaginationHelper;
 
 import java.util.List;
 
 public interface SearchDao {
-    List<SearchResult> searchProposalAttributes(PaginationHelper paginationHelper, String query);
     List<MemberSearchResult> searchMembers(PaginationHelper paginationHelper, String query);
 }
--- microservices/services/search-service/src/main/java/org/xcolab/service/search/domain/SearchDaoImpl.java
+++ microservices/services/search-service/src/main/java/org/xcolab/service/search/domain/SearchDaoImpl.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import org.xcolab.service.search.wrappers.MemberSearchResult;
-import org.xcolab.service.search.wrappers.SearchResult;
 import org.xcolab.service.utils.PaginationHelper;
 import org.xcolab.service.utils.db.jooq.fulltext.FulltextCondition;
 
 import java.util.List;
 
 import static org.xcolab.model.Tables.MEMBER;
-import static org.xcolab.model.Tables.PROPOSAL_ATTRIBUTE;
 import static org.xcolab.service.utils.db.jooq.CustomDsl.match;
 
 @Repository
@@ -21,21 +19,6 @@ public class SearchDaoImpl implements SearchDao {
 
     @Autowired
     private DSLContext dslContext;
-
-    @Override
-    public List<SearchResult> searchProposalAttributes(PaginationHelper paginationHelper,
-            String query) {
-        final FulltextCondition fulltextCondition =
-                match(PROPOSAL_ATTRIBUTE.STRING_VALUE).against(query);
-        final Field<Double> relevance = fulltextCondition.as("relevance");
-        return dslContext.select(PROPOSAL_ATTRIBUTE.STRING_VALUE.as("title"),
-                        PROPOSAL_ATTRIBUTE.STRING_VALUE.as("content"), relevance)
-                .from(PROPOSAL_ATTRIBUTE)
-                .where(fulltextCondition)
-                .orderBy(relevance.desc())
-                .limit(paginationHelper.getStartRecord(), paginationHelper.getLimitRecord())
-                .fetchInto(SearchResult.class);
-    }
 
     @Override
     public List<MemberSearchResult> searchMembers(PaginationHelper paginationHelper, String query) {
